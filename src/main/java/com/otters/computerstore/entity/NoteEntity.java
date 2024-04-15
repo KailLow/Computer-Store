e141fb56c0a808ca1bef5c10bf04fcb6cd47bc2c
KailLow <21521551@gm.uit.edu.vn>
2024-04-16 03:22:35 +0700

    create Note entity

---
+++ src/main/java/com/otters/computerstore/entity/NoteEntity.java
@@ -0,0 +1,28 @@
+package com.otters.computerstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class NoteEntity extends BaseEntity {
+    @Column(name = "note")
+    private String note;
+
+    public NoteEntity(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "NoteEntity{" +
+                "note='" + note + '\'' +
+                '}';
+    }
+}
