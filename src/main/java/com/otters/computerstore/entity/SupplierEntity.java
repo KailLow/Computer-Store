0813f2d636c69fcd64e6d33827cb659432a141c0
KailLow <21521551@gm.uit.edu.vn>
2024-04-16 03:23:23 +0700

    create supplier Entity

---
+++ src/main/java/com/otters/computerstore/entity/SupplierEntity.java
@@ -0,0 +1,35 @@
+package com.otters.computerstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class SupplierEntity extends NoteEntity{
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+
+    public SupplierEntity(String note, String name, String phone, String email, String address) {
+        super(note);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "SupplierEntity{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
