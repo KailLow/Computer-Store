06608c4d6952e7516b6f5266cdaea0b46774aff0
KailLow <21521551@gm.uit.edu.vn>
2024-04-18 02:48:11 +0700

    add product entity

---
+++ src/main/java/com/otters/computerstore/entity/ProductEntity.java
@@ -0,0 +1,39 @@
+package com.otters.computerstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class ProductEntity {
+    private String name;
+    private Long price;
+    private Integer quantity;
+    private Date warrantyPeriod;
+    private Boolean isAvailable;
+
+    public ProductEntity(String name, Long price, Integer quantity, Date warrantyPeriod, Boolean isAvailable) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.warrantyPeriod = warrantyPeriod;
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", warrantyPeriod=" + warrantyPeriod +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
