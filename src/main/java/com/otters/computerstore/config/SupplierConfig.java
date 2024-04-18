ce68010208bfabdb6c66cf204c0503069bd57e04
KailLow <21521551@gm.uit.edu.vn>
2024-04-18 16:42:57 +0700

    fix spplier config

--- src/main/java/com/otters/computerstore/config/SupplierConfig.java
+++ src/main/java/com/otters/computerstore/config/SupplierConfig.java
@@ -2,10 +2,12 @@ package com.otters.computerstore.config;
 
 import com.otters.computerstore.repository.SupplierRepo;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SupplierConfig {
+    @Bean
     CommandLineRunner commandLineRunnerSupplier(SupplierRepo supplierRepo) {
         return args -> {};
     }
